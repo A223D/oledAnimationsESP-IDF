344bc038dd3367719469441c5f44a37e791d7c2a
Kushagra Goel <64742426+A223D@users.noreply.github.com>
2022-08-13 22:47:22 -0400

    animation frames now in separate file

--- pizzaFrames/pngToBuffer.py
+++ pizzaFrames/pngToBuffer.py
@@ -1,45 +1,47 @@
-from types import new_class
 from PIL import Image
+import os
 
-im = Image.open('./frame_00_delay-0.03s.png')
-im = im.convert('RGBA')
-px = im.load()
-
-pix = list(im.getdata())
+buffer=[]
 
 WIDTH = 128
 HEIGHT = 64
 
-
-buffer = []
-
+outputString = "ui"
 
 def drawPixel(x, y, colour):
     global buffer
     byteNum = (int(y / 8) * WIDTH) + x
     newByte = buffer[byteNum]
-    if(colour ==1):
-        newByte = newByte | (1 << (y%8))
+    if (colour == 1):
+        newByte = newByte | (1 << (y % 8))
     else:
         newByte = newByte & (~(1 << (y % 8)))
 
     buffer[byteNum] = newByte
 
-#setup function
-for i in range(0, 1024):
-    buffer.append(0)
+
+files = os.listdir("./")
+print(files)
 
 
 
-for i in range(0, 64):
-    for j in range(0, 64):
-        #print("Orig ", str(i), " ", str(j))
-        if(px[i, j] != (255, 255, 255, 255)):
-            drawPixel(i+32, j, 1)
+for file in files:
+    buffer = []
+    for i in range(0, 1024):
+        buffer.append(0)
+    im = Image.open(file)
+    im = im.convert('RGBA')
+    px = im.load()
+    pix = list(im.getdata())    
+
+    for i in range(0, 64):
+        for j in range(0, 64):
+            #print("Orig ", str(i), " ", str(j))
+            if (px[i, j] != (255, 255, 255, 255)):
+                drawPixel(i + 32, j, 1)
 
 print(len(buffer))
 
-f = open("./demofile2.txt", "w")
-f.write(str(buffer))
-f.close()
-
+# f = open("./demofile2.txt", "w")
+# f.write(str(buffer))
+# f.close()
